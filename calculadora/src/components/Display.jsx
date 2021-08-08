eaa02053f80ebcb73d8ac868149ff442cddfb6a3
wilson-castro <wilsoncastro.pv@gmail.com>
2021-08-08 18:38:36 -0300

    Projeto calculadora utilizando react e css

---
+++ calculadora/src/components/Display.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import './Display.css'
+
+export default props => {
+  return (
+    <div className="display">
+      {props.value}
+    </div>
+  )
+}
