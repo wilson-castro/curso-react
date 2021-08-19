dfb4e29b5dc1850bac1d5e4af569260df84ab9de
wilson-castro <wilsoncastro.pv@gmail.com>
2021-08-19 11:01:52 -0300

    Implementação do projeto galeria utilizando de bootstrap, jquery e Webpack

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,10 @@
+//Arquivos SASS
+import './scss/index.scss'
+
+//Dependencias
+import 'jquery'
+import 'bootstrap'
+
+//Meus Arquivos JS
+import './js/core/includes'
+import './js/plugins/cityButtons'
