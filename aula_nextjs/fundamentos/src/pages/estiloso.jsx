48b7c17f0653117bc2783e5931f624896c59d6fe
wilson-castro <wilsoncastro.pv@gmail.com>
2021-09-08 10:55:32 -0300

    Primeiros testes com o nextjs

---
+++ aula_nextjs/fundamentos/src/pages/estiloso.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Estiloso.module.css'
+
+
+export default function Estiloso() {
+  return (
+    <div className={styles.roxo}>
+      <h1>Estilo usando CSS módulos</h1>
+    </div>
+  )
+}
